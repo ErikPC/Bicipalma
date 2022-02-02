b9b28739e7304ed7b7a017b1eb8ec95d9a58602c
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-02 11:54:22 +0100

    acabar clase anclaje

---
+++ bicipalma/src/main/java/edu/poniperro/domain/estacion/Anclaje.java
@@ -0,0 +1,31 @@
+package edu.poniperro.domain.estacion;
+
+import edu.poniperro.domain.bicicleta.Movil;
+
+public class Anclaje {
+
+    private boolean ocupado;
+    private Movil bici;
+
+    Anclaje(){}
+
+    boolean isOcupado() {
+        return ocupado;
+    }
+
+    Movil getBici() {
+        return bici;
+    }
+
+    void anclarBici(Movil bici){
+        this.bici = bici;
+        ocupado = true;
+    }
+    void liberarBici(){
+        ocupado = false;
+    }
+    @Override
+    public String toString(){
+        return "La " + getBici() + "esta" + isOcupado();
+    }
+}
