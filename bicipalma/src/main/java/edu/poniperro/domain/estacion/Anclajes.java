df235c714d1b78ce5c840dfffb4acc56f42ecac5
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-03 12:38:46 +0100

    hacer constructor clase anclajes

--- bicipalma/src/main/java/edu/poniperro/domain/estacion/Anclajes.java
+++ bicipalma/src/main/java/edu/poniperro/domain/estacion/Anclajes.java
@@ -1,5 +1,45 @@
 package edu.poniperro.domain.estacion;
 
+import edu.poniperro.domain.bicicleta.Movil;
+
 public class Anclajes {
 
+    private Anclaje[] anclajes; 
+
+    Anclajes(int numAnclajes){
+        this.anclajes = new Anclaje[numAnclajes];
+    }
+
+    private void crearAnclajes(){
+
+    }
+
+    Anclaje[] anclajes(){
+
+    }
+
+    int numAnclajes(){
+
+    }
+
+    void ocuparAnclaje(int , Movil id){
+
+    }
+
+    boolean isAnclajeOcupado(int){
+
+    }
+    void liberarAnclaje(int){
+
+    }
+    Movil getBiciAt(int){
+
+    }
+    int seleccionarAnclaje(){
+
+    }
+
+    public String toString(){
+        return  "La bicicleta ha sido liberada" ;
+    }
 }
