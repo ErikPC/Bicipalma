f2a958273ba66e9e3009fd701dd3efc0fdd3c7a9
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-06 20:56:08 +0100

    acabar estacion

--- bicipalma/src/main/java/edu/poniperro/domain/estacion/Estacion.java
+++ bicipalma/src/main/java/edu/poniperro/domain/estacion/Estacion.java
@@ -12,7 +12,7 @@ public class Estacion {
     public Estacion(int id, String direccion , int numAnclajes){
         this.id = id;
         this.direccion = direccion;
-        this.anclajes = numAnclajes;
+        this.anclajes = new Anclajes(numAnclajes);
         
     }
     private int getId(){
@@ -21,39 +21,68 @@ public class Estacion {
     private String getDireccion(){
         return this.direccion;
     }
+     
     @Override
     public String toString() {
         return "Es la estación con id : " + getId() + " en direccion " + getDireccion() ;
     }
+
     private Anclaje[] anclajes(){
-        return 
-    } 
-    private int numAclajes(){
-        return
+        return anclajes.anclajes();
     }
+
+    private int numAclajes(){
+        return anclajes.numAnclajes();
+    }
+
     public void consultarEstacion(){
 
     }
     public int anclajesLibres(){
-        return
-    }
-    public void anclarBicicleta(){
+        int anclajesLibres = 0;
+        for (int i = 0; i < numAclajes() ; i++){
+            if (!anclajes.isAnclajeOcupado(i)){
+                anclajesLibres++;
+            }
+        }
+        return anclajesLibres;
 
     }
-    public boolean leerTarjetaUsuario(Autentication activada){
-        return
-    }
-    public void SretirarBicicleta(Autentication id){
 
-    }
-    private void mostrarBocicleta( Movil , int ){
 
+    public void anclarBicicleta(Movil bici){
+        if (anclajesLibres() >= 0){
+        anclajes.ocuparAnclaje(anclajes.seleccionarAnclaje() , bici);
+    } else {
+        System.out.println("Todos los anclajes estan ocupado");
     }
-    private void mostrarAnclaje(Movil , int) {
+    }
 
+    public boolean leerTarjetaUsuario(Autentication tarjeta){
+        return tarjeta.isActivada();
+    }
+
+    public void retirarBicicleta(Autentication tarjeta){
+        int anclaje = anclajes.seleccionarAnclajeOcupado();
+        if(leerTarjetaUsuario(tarjeta)){
+            anclajes.liberarAnclaje(anclaje);
+            mostrarBicicleta(anclajes.getBiciAt(anclaje), anclaje);
+        }
+    }
+    private void mostrarBicicleta(Movil bici , int anclaje){
+        System.out.println(bici + " "+ anclaje);
+    }
+
+    private void mostrarAnclaje(Movil bici, int anclaje){
+        System.out.println(bici + " "+ anclaje);
     }
     public void consultarAnclajes(){
-        
+        for (int i = 0 ; i < numAclajes(); i++){
+            if (!anclajes.isAnclajeOcupado(i)){
+                mostrarAnclaje(anclajes.getBiciAt(i), i);
+            } else{
+                System.out.println("El anclaje esta libre");
+            }
+        }
     }
-
 }
